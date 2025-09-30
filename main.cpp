a3724b4697e50aae439a6e4e6c24ed3ca4787c7b
Anthony Alberto <2023260043@ub.edu.bz>
2025-09-30 17:32:25 +0000

    Added struct to file

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct BinaryTreeNode {
+	string data;
+	BinaryTreeNode* left;
+	BinaryTreeNode* right;
+
+	BinaryTreeNode(cinst string& value) {
+		data = value;
+		left = nullptr;
+		right = nullptr;
+	}
+};
+
+int main() {
+
+	return 0;
+}
