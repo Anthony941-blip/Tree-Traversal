2d41d37a419f71e259076e6b19416d649468c75e
Anthony Alberto <2023260043@ub.edu.bz>
2025-09-30 18:04:48 +0000

    Added postOrder function

--- main.cpp
+++ main.cpp
@@ -13,7 +13,19 @@ struct BinaryTreeNode {
 	}
 };
 
+void postorder(BinaryTreeNode* root);
+
 int main() {
 
 	return 0;
 }
+
+void postorder(BinaryTreeNode* root) { 
+        if(root == nullptr) { 
+                return;
+        }
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->data << " ";
+}
+
